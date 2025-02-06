81773e34737826f6dccdc0d3be0a123dd6950046
Haider Habeeb <coba2003x@gmail.com>
2025-02-06 18:47:49 +0300

    Add GetX state management and refactor main app structure

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TabaraApp extends StatelessWidget {
+  const TabaraApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
